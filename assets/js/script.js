f00f89311293b60382208a6b4d5d78053642f4d9
codemasterdev <skillz1029@gmail.com>
2021-12-06 15:07:28 -0500

    removed lorem text

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,4 +184,63 @@ $("#remove-tasks").on("click", function () {
 // load tasks for the first time
 loadTasks();
 
+// sortables active for moving task around
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerence: "pointer",
+  helper: "clone",
+  active: function (event) {
+    console.log("active", this)
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    //loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp arrat as an object
+      tempArr.push({
+        text: text,
+        date: date
+
+      });
+
+      // trim down list's ID to match object property
+      var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+      console.log(tempArr);
+
+
+
+
+    });
+  }
+});
 
